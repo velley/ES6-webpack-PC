125de8ad1e28da1f51559bdc61f7edd5bee1475d
velley <fanxing1993@qq.com>
2018-08-31 17:20:36 +0800

    8.31c

--- src/pages/script/index.js
+++ src/pages/script/index.js
@@ -1,9 +1,6 @@
 import 'style/index.styl'
 import api from 'api/user.js'
-// import 'babel-polyfill'
-api.checkLogin().then((res)=>{    
-    console.log(res)
-})
 
-console.log(Promise)
+
+
 
--- src/public/index.js
+++ src/public/index.js
@@ -1,8 +1,5 @@
-import './default.styl' //引入stylus默认配置项
-
-
-const AI = {
-    find:function() {
-        console.log('here')
-    }
-}
+import './default.styl'                //引入stylus默认样式
+import 'semantic-ui/dist/semantic.css' //引入UI样式库
+import 'semantic-ui/dist/semantic.js'
+import './nav.styl'                    //引入网页导航栏样式及JS逻辑
+import './nav.js'
